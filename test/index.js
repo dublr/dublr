cbab3b894ac50347d7790167f8e27926792ee541
Hiroshi Yamamoto <hiroshi.yamamoto.dublr@protonmail.com>
2022-08-14 09:30:11 -0600

    Improve Multichain test

--- test/index.js
+++ test/index.js
@@ -495,10 +495,12 @@ describe("OmniToken", () => {
     await contract0._owner_authorizeMultichainRouter(wallet[0].address, true);
     await contract0["mint(address,uint256)"](wallet[0].address, 1000);
     expect(await contract0.balanceOf(wallet[0].address)).to.equal(3000);
+    await contract0["burn(address,uint256)"](wallet[0].address, 500);
+    expect(await contract0.balanceOf(wallet[0].address)).to.equal(2500);
     await contract0._owner_enableMultichainRouting(false);
     await expect(contract0["mint(address,uint256)"](wallet[0].address, 1000))
             .to.be.revertedWith("Not authorized");
-    expect(await contract0.balanceOf(wallet[0].address)).to.equal(3000);
+    expect(await contract0.balanceOf(wallet[0].address)).to.equal(2500);
   });
 
 });
